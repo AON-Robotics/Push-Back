a2aa1a1885147e259fab1677d1fcb798377147fd
Pablo <dieppa.pablo25@ccndpr.com>
2026-03-04 11:05:38 -0400

    Auton routine issue fixed

--- include/aon/competition/autonomous-routines.hpp
+++ include/aon/competition/autonomous-routines.hpp
@@ -992,9 +992,7 @@ int SkillsBlackBotKevin(){
   drivetrain.goTo(-.9, -.9);
   driveIntoRing();
   drivetrain.move(-24);
-
-  -
-      // Grab ring in (0, --1.2)
+  // Grab ring in (0, --1.2)
   drivetrain.turnTo(0, -1.2);
   
   return 0;
