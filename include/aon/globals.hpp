e0629630879639d9b69cdf52941687734bfb7ccf
Kevin Gomez <kevin.gomez6@upr.edu>
2026-04-21 23:53:37 -0400

    updated intake port in the small bot

--- include/aon/globals.hpp
+++ include/aon/globals.hpp
@@ -44,7 +44,7 @@ aon::Odometry odometry = aon::Odometry(19, -18, 5, 0, 16);
 
 aon::TankDrive drivetrain = aon::TankDrive({11, -12, 13, -14}, {1, -2, 3, -4}, std::make_unique<aon::Odometry>(odometry));
 
-aon::Intake intake = aon::Intake({-9, 6}, {7}, {-8}, 'H', 'B', 'A', 20, 17);
+aon::Intake intake = aon::Intake({-9, -6}, {7}, {-8}, 'H', 'B', 'A', 20, 17);
 
 aon::Piston arrow('C', aon::Piston::RETRACTED);
 aon::Piston brooks('G', aon::Piston::RETRACTED);
